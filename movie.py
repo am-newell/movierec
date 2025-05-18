cca9d049b0deab9c63e8e9c1dbbc0a27c3e41349
minion12345678 <alexis.newell@ucalgary.ca>
2025-05-17 20:17:00 -0600

    filtering

--- movie.py
+++ movie.py
@@ -7,5 +7,13 @@ column_names = ['user_id', 'item_id', 'rating', 'timestamp']
 path = 'file.tsv'
 
 df = pd.read_csv(path, sep='\t', names=column_names)
+movie_titles = pd.read_csv('Movie_Id_Titles.csv')
+data = pd.merge(df, movie_titles, on='item_id')
+data.groupby('title')['rating'].mean().sort_values(ascending=False).head()
+ratings = pd.DataFrame(data.groupby('title')['rating'].mean()) 
 
-print(df.head())
+ratings['num of ratings'] = pd.DataFrame(data.groupby('title')['rating'].count())
+
+
+print(ratings.head())
+
