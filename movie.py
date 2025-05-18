b510b69dc15dcc3f037a105cb7c9398b613542b9
minion12345678 <alexis.newell@ucalgary.ca>
2025-05-17 22:27:19 -0600

    working v1

--- movie.py
+++ movie.py
@@ -13,7 +13,27 @@ data.groupby('title')['rating'].mean().sort_values(ascending=False).head()
 ratings = pd.DataFrame(data.groupby('title')['rating'].mean()) 
 
 ratings['num of ratings'] = pd.DataFrame(data.groupby('title')['rating'].count())
+plt.figure(figsize =(10, 4))
 
+ratings['rating'].hist(bins = 70)
+#plt.show()
 
-print(ratings.head())
+moviemat = data.pivot_table(index ='user_id',
+              columns ='title', values ='rating')
+
+ratings.sort_values('num of ratings', ascending = False).head(10)
+
+starwars_user_ratings = moviemat['Star Wars (1977)']
+liarliar_user_ratings = moviemat['Liar Liar (1997)']
+similar_to_starwars = moviemat.corrwith(starwars_user_ratings)
+similar_to_liarliar = moviemat.corrwith(liarliar_user_ratings)
+
+corr_starwars = pd.DataFrame(similar_to_starwars, columns =['Correlation'])
+corr_starwars.dropna(inplace = True)
+corr_starwars.sort_values('Correlation', ascending = False).head(10)
+corr_starwars = corr_starwars.join(ratings['num of ratings'])
+
+print(corr_starwars.head())
+
+corr_starwars[corr_starwars['num of ratings']>100].sort_values('Correlation', ascending = False).head()
 
